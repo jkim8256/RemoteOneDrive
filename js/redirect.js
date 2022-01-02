1a6e7d9d65a98234153c4c36b7bea7fd761377b7
jkim8256 <jjkim8256@gmail.com>
2022-01-01 19:48:13 -0500

    added redirect

---
+++ js/redirect.js
@@ -0,0 +1,16 @@
+const task = document.querySelector(".task");
+const input = task.getElementsByID();
+
+task.addEventListener(submit, handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault();
+    if input == 
+    location.href = `html/{sdf}.html`;
+    
+}
+
+document.querySelector(task).onclick = function () {
+    task.get
+    location.href = "html/packaging.html";
+};
